3adde368b099f24545cdf37f43508704ab4b884b
sooin <lilesooin@gmail.com>
2026-03-02 12:08:58 -0500

    GetInvalidEntryCount

--- C# review assignment/Lab5/Lab5/Lab5.cs
+++ C# review assignment/Lab5/Lab5/Lab5.cs
@@ -18,8 +18,6 @@ namespace Lab5
                 if (usersPerDay[i] > 1000)
                 {
                     double revenue = (int)((245743 + usersPerDay[i] / 4.0) * 100 + 0.5) / 100.0;
-                    Console.WriteLine($"revenue: {revenue}");
-
                     if (revenuePerDay[i] != revenue)
                     {
                         revenuePerDay[i] = revenue;
@@ -29,8 +27,6 @@ namespace Lab5
                 else if (usersPerDay[i] > 100)
                 {
                     double revenue = (int)((usersPerDay[i] * usersPerDay[i] / 4.0 - 2 * usersPerDay[i] - 2007) * 100 + 0.5) / 100.0;
-
-                    Console.WriteLine($"revenue: {revenue}");
                     if (revenuePerDay[i] != revenue)
                     {
                         revenuePerDay[i] = revenue;
@@ -40,7 +36,6 @@ namespace Lab5
                 else if (usersPerDay[i] > 10)
                 {
                     double revenue = (int)((16 * usersPerDay[i] / 5.0 - 27) * 100 + 0.5) / 100.0;
-                    Console.WriteLine($"revenue: {revenue}");
                     if (revenuePerDay[i] != revenue)
                     {
                         revenuePerDay[i] = revenue;
@@ -50,8 +45,6 @@ namespace Lab5
                 else
                 {
                     double revenue = (int)((usersPerDay[i] / 2.0) * 100 + 0.5) / 100.0;
-                    Console.WriteLine($"revenue: {revenue}");
-
                     if (revenuePerDay[i] != revenue)
                     {
                         revenuePerDay[i] = revenue;
@@ -65,12 +58,59 @@ namespace Lab5
 
         public static int GetInvalidEntryCount(uint[] usersPerDay, double[] revenuePerDay)
         {
-            return -1;
+            if (usersPerDay.Length != revenuePerDay.Length)
+            {
+                return -1;
+            }
+
+            int count = 0;
+            double[] expectedRevenue = new double[revenuePerDay.Length];
+           
+            for (int i = 0; i < usersPerDay.Length; ++i)
+            {
+                if (usersPerDay[i] > 1000)
+                {
+                    expectedRevenue[i] = 245743 + usersPerDay[i] / 4.0;
+                }
+                else if (usersPerDay[i] > 100)
+                {
+                    expectedRevenue[i] = usersPerDay[i] * usersPerDay[i] / 4.0 - 2 * usersPerDay[i] - 2007;
+                }
+                else if (usersPerDay[i] > 10)
+                {
+                    expectedRevenue[i] = 16 * usersPerDay[i] / 5.0 - 27;
+                }
+                else
+                {
+                    expectedRevenue[i] = usersPerDay[i] / 2.0;
+                }
+
+                if (revenuePerDay[i] != expectedRevenue[i])
+                {
+                    revenuePerDay[i] = (int)(expectedRevenue[i] * 100 + 0.5) / 100.0;
+                    ++count;
+                }
+            }
+
+            return count;
         }
 
         public static double CalculateTotalRevenue(double[] revenuePerDay, uint start, uint end)
         {
-            return 0.0;
+            if (revenuePerDay.Length == 0 || start >= end || end > revenuePerDay.Length - 1)
+            {
+                return -1;
+            }
+
+            double totalRevenue = 0.0;
+
+            for (uint i = start; i <= end; ++i)
+            {
+                totalRevenue += revenuePerDay[i];
+            }
+       
+            Console.WriteLine("totalRevenue > " + totalRevenue);
+            return totalRevenue;
         }
     }
 }
